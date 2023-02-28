87408f3e4187ff980ce6647fc75f2928134ec4a4
fred39392001 <fred39392001@gmail.com>
2023-02-28 15:24:34 +0800

    Update About2ndHome.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="bg-yellow">
+    <div class="container">
+        <div class="row py-4 border-bottom border-secondary">
+          <div class="col-lg-8 d-none d-lg-block">
+            <div class="h-100 d-flex align-items-center justify-content-center justify-content-lg-start mt-2 mt-lg-0">
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">關於 Second Home</router-link></p>
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">永續專案</router-link></p>
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">解決方案</router-link></p>
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">最新消息</router-link></p>
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">聯絡我們</router-link></p>
+              <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">隱私權條款</router-link></p>
+            </div>
+          </div>
+          <div class="col-lg-4 mb-4 mb-lg-0">
+            <div class="d-flex h-100 align-items-center justify-content-center justify-content-lg-end">
+              <div>
+                <router-link to="" target="_blank">
+                  <img class="me-1" src="img/townWay-icon.svg" alt="" style="height: 35px;">
+                </router-link>
+                <router-link to="" target="_blank">
+                  <img class="me-3" src="img/fb-icon.svg" alt="" style="height: 25px;">
+                </router-link>
+                <router-link to="" target="_blank">
+                  <img class="me-3" src="img/linkedin-icon.svg" alt="" style="height: 25px;">
+                </router-link>
+                <router-link to="" target="_blank">
+                  <img class="me-3" src="img/instagram-icon.svg" alt="" style="height: 25px;">
+                </router-link>
+                <router-link to="" target="_blank">
+                  <img src="img/youtube-icon.svg" alt="" style="height: 25px;">
+                </router-link>
+              </div>
+            </div>
+          </div>
+          <!-- mobile -->
+          <div class="d-flex flex-wrap justify-content-center d-lg-none">
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">關於 Second Home</router-link></p>
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">永續專案</router-link></p>
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">解決方案</router-link></p>
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">最新消息</router-link></p>
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">聯絡我們</router-link></p>
+            <p class="me-3 mb-0"><router-link class="text-dark nav-link fw-bold" to="">隱私權條款</router-link></p>
+          </div>
+        </div>
+        <div class="row py-4">
+          <div class="d-md-flex h-100 align-items-center justify-content-center">
+            <p class="mb-0 text-center fw-bold">Copyright &copy; 2022 SECOND HOME</p>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-warning bg-yellow py-md-4">
+    <div class="container">
+      <router-link class="navbar-brand d-none d-md-block my-md-1" to="/">
+        <img class=" logo-pc" src="img/2nd-home.png" alt="">
+      </router-link>
+      <router-link class="navbar-brand d-md-none" to="/">
+        <img class=" logo-mobile" src="img/2nd-home.png" alt="">
+      </router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="">關於 Second Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="">永續專案</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="">解決方案</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="">最新消息</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to="">連絡我們</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link fw-bold" to=""><img class="align-top" src="img/user.svg" alt="">登入/註冊</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+}
+</script>
